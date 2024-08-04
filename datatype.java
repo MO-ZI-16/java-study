92d42d7981cafb31e187954586445dbc2d2eb796
MO-ZI-16 <MO-ZI-16@outlook.com>
2024-08-04 22:20:42 +0800

    1.java数据类型

---
+++ datatype.java
@@ -0,0 +1,56 @@
+/*public class datatype{
+    public static void main(String[] args) {
+       //定义变量记录姓名
+        String name="黑马";
+       //定义变量记录年龄
+        int age=18;
+       //定义变量记录性别
+        char gender='男';
+        //定义变量记录身高
+        double height=180.2;
+        //定义变量是否单身
+        //true 单身,false 不是单身
+        boolean flag=true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+        System.out.println(flag);
+
+    }
+}*/
+
+/* 
+public class datatype{
+    public static void main(String[] args) {
+       //定义变量电影名字
+        String name="送初恋回家";
+       //定义变量记录年龄
+       String tostar="刘曦 张雨提";
+       //定义变量记录年份
+        int year=2020;
+        //定义变量记录身高
+        double grade=9.2;
+
+
+        System.out.println(name);
+        System.out.println(tostar);
+        System.out.println(year);
+        System.out.println(grade);
+
+    }
+}*/
+
+public class datatype{
+    public static void main(String[] args) {
+       //定义变量手机价格
+        double price=5299.0;
+       //定义变量记录手机品牌
+       String name="华为";
+       
+        //输出结果
+        System.out.println(name);
+        System.out.println(price);
+    }
+}
